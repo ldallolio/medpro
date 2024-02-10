98aa6b14354e88b5d2d556f9b615f784d496bed4
Luca Dall'Olio <luca.dallolio@aego.ai>
2024-02-10 11:11:11 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+===============================
+medpro
+===============================
+
+
+.. image:: https://img.shields.io/travis/ldallolio/medpro.svg
+        :target: https://travis-ci.org/ldallolio/medpro
+.. image:: https://circleci.com/gh/ldallolio/medpro.svg?style=svg
+    :target: https://circleci.com/gh/ldallolio/medpro
+.. image:: https://codecov.io/gh/ldallolio/medpro/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/ldallolio/medpro
+
+
+Medcoupling based post processing tools
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    # package requirements go here
+]
+
+setup(
+    name='medpro',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Medcoupling based post processing tools",
+    license="MIT",
+    author="Luca Dall Olio",
+    author_email='luca.dallolio@gmail.com',
+    url='https://github.com/ldallolio/medpro',
+    packages=['medpro'],
+    
+    install_requires=requirements,
+    keywords='medpro',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
