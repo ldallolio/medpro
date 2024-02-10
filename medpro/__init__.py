d609c679a53edaf03f6ea28c9ae8b3a9765cf04b
Luca Dall'Olio <luca.dallolio@aego.ai>
2024-02-10 18:48:34 +0100

    adding extract group, tests ok

--- medpro/__init__.py
+++ medpro/__init__.py
@@ -3,6 +3,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 import medcoupling as mc
+import os
 
 from .mesh import *
 from .param import *
@@ -10,24 +11,30 @@ from .field import *
 
 class MEDFilePost():
 
-    def __init__(self, file_name):
-        self.file_data = mc.MEDFileData.New(file_name)
+    def __init__(self, file_name = None):
+        if file_name is not None:
+            self.file_data = mc.MEDFileData.New(file_name)
+        else:
+            self.file_data = mc.MEDFileData.New()
 
         self.meshes_by_name = {}
-        if self.file_data.getNumberOfMeshes() >= 1:
+        if file_name is not None and self.file_data.getNumberOfMeshes() >= 1:
             for file_mesh in self.file_data.getMeshes():
                 self.meshes_by_name[file_mesh.getName()] = MEDMesh(file_mesh)
 
         self.fieldevols_by_name = {}
-        if self.file_data.getNumberOfFields() >= 1:
+        if file_name is not None and self.file_data.getNumberOfFields() >= 1:
             for field_file in self.file_data.getFields():
                 mesh = self.meshes_by_name[field_file.getMeshName()]
-                self.fieldevols_by_name[field_file.getName()] = MEDFieldEvol(field_file, mesh)
+                self.fieldevols_by_name[field_file.getName()] = MEDFieldEvol(mesh, field_file)
 
         self.params_by_name = {}
-        if self.file_data.getNumberOfParams() >= 1: # to avoid MEDLoader.InterpKernelException: MEDFileParameters::getParamAtPos : should be in [0,0)
+        if file_name is not None and self.file_data.getNumberOfParams() >= 1: # to avoid MEDLoader.InterpKernelException: MEDFileParameters::getParamAtPos : should be in [0,0)
             for param in self.file_data.getParams():
                 self.fields_by_name[param.getName()] = MEDParam(param)
+
+    def add_fieldevol(self, field_evol):
+        self.fieldevols_by_name[field_evol.name] = field_evol
 
     def write(self, output_file_name):
         for mesh_index, mesh in enumerate(self.meshes_by_name.values()):
