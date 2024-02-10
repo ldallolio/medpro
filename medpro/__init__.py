79cd082cccaef7062d3a5e0a1ec43b9b62d07aef
Luca Dall'Olio <luca.dallolio@aego.ai>
2024-02-10 16:04:11 +0100

    better field manipulation (keeping mesh informations)

--- medpro/__init__.py
+++ medpro/__init__.py
@@ -10,28 +10,32 @@ import MEDLoader as ml
 from medcoupling import ON_CELLS, ON_NODES, ON_GAUSS_PT, ON_GAUSS_NE, ON_NODES_KR, ON_NODES_FE
 
 class MEDMesh():
-    def __init__(self, file_mesh):
-        self.file_mesh = file_mesh
+    def __init__(self, mesh_file):
+        self.mesh_file = mesh_file
 
     @property
     def name(self):
-        return self.file_mesh.getName()
+        return self.mesh_file.getName()
     
     @property
     def group_names(self):
-        return self.file_mesh.getGroupsNames()
+        return self.mesh_file.getGroupsNames()
     
     @property
     def num_nodes(self):
-        return self.file_mesh.getNumberOfNodes()
+        return self.mesh_file.getNumberOfNodes()
     
     @property
     def mesh_dim(self):
-        return self.file_mesh.getMeshDimension()
+        return self.mesh_file.getMeshDimension()
     
     @property
     def space_dim(self):
-        return self.file_mesh.getSpaceDimension()    
+        return self.mesh_file.getSpaceDimension()
+
+    @property
+    def coordinates(self):
+        return self.mesh_file.getCoords().toNumPyArray()   
 
 @dataclass(frozen=True)
 class TimeStamp():
@@ -40,8 +44,9 @@ class TimeStamp():
     time: float
 
 class MEDField():
-    def __init__(self, field):
+    def __init__(self, field, mesh_file):
         self.field = field
+        self.mesh = mesh_file
 
     @property
     def name(self):
@@ -49,32 +54,41 @@ class MEDField():
 
     @property
     def on_nodes(self):
-        return any(t == mc.ON_NODES for t in self.field.getTypesOfFieldAvailable())
+        return self.field.getTypeOfField() == mc.ON_NODES
 
     @property
     def on_cells(self):
-        return any(t == mc.ON_CELLS for t in self.field.getTypesOfFieldAvailable())
+        return self.field.getTypeOfField() == mc.ON_CELLS
 
     @property
     def on_gauss_points(self):
-        return any(t == mc.ON_GAUSS_PT for t in self.field.getTypesOfFieldAvailable())
+        return self.field.getTypeOfField() == mc.ON_GAUSS_PT
 
     @property
     def on_nodes_per_element(self):
-        return any(t == mc.ON_GAUSS_NE for t in self.field.getTypesOfFieldAvailable())
+        return self.field.getTypeOfField() == mc.ON_GAUSS_NE
 
     @property
     def timestamp(self):
-        iteration, order, time = self.field.getTime()
+        time, iteration, order = self.field.getTime()
         return TimeStamp(iteration, order, time)
     
     @property
     def components(self):
-        return self.field.getInfo()    
+        return self.field.getArray().getInfoOnComponents()    
+
+    @property
+    def units(self):
+        return self.field.getArray().getUnitsOnComponents()
+    
+    @property
+    def to_numpy(self):
+        return self.field.getArray().toNumPyArray()
 
 class MEDFieldEvol():
-    def __init__(self, file_field):
+    def __init__(self, file_field, mesh):
         self.file_field = file_field
+        self.mesh = mesh
 
     @property
     def name(self):
@@ -95,13 +109,29 @@ class MEDFieldEvol():
     @property
     def timesteps(self):
         return [TimeStamp(iteration, order, time) for iteration, order, time in self.file_field.getTimeSteps()]
+    
+    def __build_field(self, field_1ts):
+        assert len(self.file_field.getTypesOfFieldAvailable()) == 1
+        assert len(self.file_field.getTypesOfFieldAvailable()[0]) == 1
+        field_type = self.file_field.getTypesOfFieldAvailable()[0][0] # TODO understand this and make it more general
+        field_vals, depl_prf = field_1ts.getFieldWithProfile(field_type, 0, self.mesh.mesh_file)
+
+        whole_mesh = self.mesh.mesh_file.getMeshAtLevel(0)
+        double_field = mc.MEDCouplingFieldDouble.New(field_type, mc.ONE_TIME)
+        double_field.setName(field_1ts.getName())
+        double_field.setMesh(whole_mesh)
+        double_field.setArray(field_vals)
+        
+        iteration, order, time = field_1ts.getTime()
+        double_field.setTime(time, iteration, order)
+        return MEDField(double_field, self.mesh)
        
     def get_field_at_timestep(self, iteration, order):
-        return MEDField(self.file_field.getTimeStep(iteration, order))
+        return self.__build_field(self.file_field.getTimeStep(iteration, order))
     
     @property
     def fields_by_timestep(self):
-        return {TimeStamp(*field.getTime()): MEDField(field) for field in self.file_field}
+        return {TimeStamp(*field.getTime()): self.__build_field(field) for field in self.file_field}
 
     def extract_group(self, group_name: str):
         pass
@@ -117,13 +147,14 @@ class MEDFilePost():
 
         self.meshes_by_name = {}
         if self.file_data.getNumberOfMeshes() >= 1:
-            for mesh_file in self.file_data.getMeshes():
-                self.meshes_by_name[mesh_file.getName()] = MEDMesh(mesh_file)
+            for file_mesh in self.file_data.getMeshes():
+                self.meshes_by_name[file_mesh.getName()] = MEDMesh(file_mesh)
 
         self.fieldevols_by_name = {}
         if self.file_data.getNumberOfFields() >= 1:
             for field_file in self.file_data.getFields():
-                self.fieldevols_by_name[field_file.getName()] = MEDFieldEvol(field_file)
+                mesh = self.meshes_by_name[field_file.getMeshName()]
+                self.fieldevols_by_name[field_file.getName()] = MEDFieldEvol(field_file, mesh)
 
         self.params_by_name = {}
         if self.file_data.getNumberOfParams() >= 1: # to avoid MEDLoader.InterpKernelException: MEDFileParameters::getParamAtPos : should be in [0,0)
--- tests/test_medinfos.py
+++ tests/test_medinfos.py
@@ -1,6 +1,6 @@
 import medpro
 
-def test_file_inspect():
+def test_mesh():
     fp = medpro.MEDFilePost("./tests/examples/box_with_depl.rmed")
     assert len(fp.params_by_name) == 0
     assert len(fp.meshes_by_name) == 1
@@ -12,6 +12,9 @@ def test_file_inspect():
     assert mesh.num_nodes == 27
     assert mesh.mesh_dim == 3
     assert mesh.space_dim == 3
+
+def test_field_evol():
+    fp = medpro.MEDFilePost("./tests/examples/box_with_depl.rmed")
 
     assert len(fp.fieldevols_by_name) == 2
     assert "reslin__DEPL" in fp.fieldevols_by_name
@@ -30,6 +33,14 @@ def test_file_inspect():
     assert depl_evol.timesteps[0].order == 1
     assert depl_evol.timesteps[0].time == 0.0
 
+def test_field():
+    fp = medpro.MEDFilePost("./tests/examples/box_with_depl.rmed")
+
+    assert len(fp.fieldevols_by_name) == 2
+    assert "reslin__DEPL" in fp.fieldevols_by_name
+    
+    depl_evol = fp.fieldevols_by_name['reslin__DEPL']
+
     depl = depl_evol.get_field_at_timestep(1, 1)
     assert depl.name == 'reslin__DEPL'
     assert depl.on_nodes  
@@ -39,7 +50,8 @@ def test_file_inspect():
     assert len(depl.components) == 3
     assert "DX" in depl.components
     assert "DY" in depl.components
-    assert "DZ" in depl.components    
+    assert "DZ" in depl.components
+    assert depl.to_numpy.size == depl.mesh.num_nodes * len(depl.components)
 
 def test_extract_group():
     pass
